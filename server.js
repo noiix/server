01b9102f98955838d50883716361c6f57e1f1b0a
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-10-19 16:45:17 +0200

    ..

--- server.js
+++ server.js
@@ -96,6 +96,9 @@ io.on("connection", (socket) => {
     console.log("user joined room: " + room)
   });
 
+  socket.on('typing', (room) => socket.in(room).emit('typing'));
+  socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
+
   socket.on('new message', (newMessageReceived) => {
     let chat = newMessageReceived.chat;
 
@@ -105,5 +108,9 @@ io.on("connection", (socket) => {
       // if(user._id === newMessageReceived.sender._id) return;
       socket.in(user._id).emit('message received', newMessageReceived);
   })
+  });
+  socket.off('setup', () => {
+    console.log('USER DISCONNECTED');
+    socket.leave(userData._id);
   })
 })
