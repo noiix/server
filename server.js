dd4ccca31a73ed8fa0acacc3e0ba94de7dde37b2
Fridabug <friederikespring@posteo.de>
2022-10-25 12:24:23 +0200

    .

--- server.js
+++ server.js
@@ -96,20 +96,28 @@ io.on("connection", (socket) => {
     console.log("user joined room: " + room)
   });
 
+  socket.on('typing', (room) => {
+    socket.in(room).emit('typing')
+  })
+
+  socket.on('stop typing', (room) => {
+    socket.in(room).emit('stop typing')
+  })
+  
+
   socket.on('new message', (newMessageReceived) => {
     let chat = newMessageReceived.chat;
 
     if(!chat.users) return console.log("chat.users not defined")
 
     chat.users.forEach(user => {
-<<<<<<< HEAD
-      if(user._id === newMessageReceived.sender._id) return;
-      socket.in(user._id).emit('message', newMessageReceived);
-    })
-=======
       // if(user._id === newMessageReceived.sender._id) return;
       socket.in(user._id).emit('message received', newMessageReceived);
   })
->>>>>>> f4d2c8b53714dac62c0d97d0e820e13d22a617ff
+  })
+
+  socket.off('setup', () => {
+    console.log('USER DISCONNECTED');
+    socket.leave(userData._id)
   })
 })
