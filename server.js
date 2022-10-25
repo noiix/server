fdfe00ceefcf73a4e3d1a5afa19d8e4a77f70e56
Fridabug <friederikespring@posteo.de>
2022-10-25 13:21:03 +0200

    .

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on('typing', (room) => {
-    socket.in(room).emit('typing')
+    socket.in(room).emit('typing', room)
   })
 
   socket.on('stop typing', (room) => {
