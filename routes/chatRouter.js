bacb03db270b0d96aab1b5524b1b9b9b1e6cf90f
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-11-01 13:16:13 +0100

    new default image in user model

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {auth} = require('../middleware/checkLoggedIn')
-const {accessChat, fetchChats} = require('../controllers/chatController')
+const {accessChat, fetchChats, initialChatBot} = require('../controllers/chatController')
 
 
 // router.get('/', (req, res) => {
@@ -14,5 +14,6 @@ const {accessChat, fetchChats} = require('../controllers/chatController')
 
 router.post('/', auth, accessChat);
 router.get('/', auth, fetchChats);
+router.get('/chatbot', auth, initialChatBot)
 
 module.exports = router;
