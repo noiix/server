3f1f930bf9c4231bd3687df46cf28612fe5acf61
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-11-03 14:41:56 +0100

    fetch all messages for the chat teaser

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -14,6 +14,6 @@ const {accessChat, fetchChats, initialChatBot} = require('../controllers/chatCon
 
 router.post('/', auth, accessChat);
 router.get('/', auth, fetchChats);
-router.get('/chatbot', auth, initialChatBot)
+// router.get('/chatbot', auth, initialChatBot)
 
 module.exports = router;
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {auth} = require('../middleware/checkLoggedIn')
-const {sendMessage, allMessages, setMessageToRead, initialMessage} = require('../controllers/messageController')
+const {sendMessage, allMessages, setMessageToRead, fetchAllMessages} = require('../controllers/messageController')
  
 router.post('/', auth, sendMessage);
+router.get('/all', auth, fetchAllMessages);
 router.get('/:chatId', auth, allMessages);
 router.patch('/read', auth, setMessageToRead);
-router.post('/initialmessage', auth, initialMessage)
+// router.post('/initialmessage', auth, initialMessage)
 
 module.exports = router;
