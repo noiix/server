59bdd6ae1fd564cc8b5c9f67e6af48e5e03be886
eszterk90git config --global user.email eszter.kadar1@gmail.com <eszter.kadar1@gmail.com>
2022-11-06 21:28:37 +0100

    custom url added to cors origin

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cors({origin: 'https://noix.onrender.com', credentials: true}));
+app.use(cors({origin: 'https://client.noix.space', credentials: true}));
 app.use(cookieParser());
 
 app.use(errorController);
